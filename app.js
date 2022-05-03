e6cf6f8102ad43bcc606459d4b57790ea2cb907e
HungyeeLinS3790956 <S3790956@student.rmit.edu.au>
2022-05-03 15:54:49 +0800

    adding statistic detail into modal

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 window.onload=()=>{
-    const btn_instruction = document.querySelector('#modal_instruction')
-    btn_instruction.click()
+    // const btn_instruction = document.querySelector('#modal_instruction')
+    // btn_instruction.click()
     
+    const btn_statistic = document.querySelector('#modal_statistic')
+    btn_statistic.click()
 }
